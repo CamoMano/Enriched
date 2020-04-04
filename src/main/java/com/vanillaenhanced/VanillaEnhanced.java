e2a23004977e6f8a8b6b61b73a7044cea574da43
CamoMano <camomano99@gmail.com>
2020-04-03 21:29:02 -0400

    Added armor

--- src/main/java/com/vanillaenhanced/VanillaEnhanced.java
+++ src/main/java/com/vanillaenhanced/VanillaEnhanced.java
@@ -1,8 +1,11 @@
 package com.vanillaenhanced;
 
+import com.vanillaenhanced.materials.items.ArmorMaterialObsidian;
 import com.vanillaenhanced.materials.items.ToolMaterialObsidian;
 import net.fabricmc.api.ModInitializer;
 import com.vanillaenhanced.items.*;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.Level;
@@ -15,15 +18,21 @@ public class VanillaEnhanced implements ModInitializer {
 
     public static final String MOD_ID = "vanillaenhanced";
     public static final String MOD_NAME = "VanillaEnhanced";
+    public static final ArmorMaterial OBSIDIAN_ARMOR = new ArmorMaterialObsidian();
 
     @Override
     public void onInitialize() {
-        log(Level.INFO, "Initializing");
+        log(Level.INFO, "Hello there.");
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_pickaxe"), new PickaxeBase(new ToolMaterialObsidian()));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_axe"), new AxeBase(new ToolMaterialObsidian()));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_shovel"), new ShovelBase(new ToolMaterialObsidian()));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_sword"), new SwordBase(new ToolMaterialObsidian()));
         Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_hoe"), new HoeBase(new ToolMaterialObsidian()));
+
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_helmet"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.HEAD));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_chestplate"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.CHEST));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_leggings"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.LEGS));
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_boots"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.FEET));
 
     }
 
---
+++ src/main/java/com/vanillaenhanced/materials/items/ArmorMaterialObsidian.java
@@ -0,0 +1,51 @@
+package com.vanillaenhanced.materials.items;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class ArmorMaterialObsidian implements ArmorMaterial {
+
+
+    private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
+    private static final int[] PROTECTION_AMOUNTS = new int[]{4, 8, 6, 4};
+
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()]*35;
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_AMOUNTS[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 9;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_GOLD;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.OBSIDIAN);
+    }
+
+    @Override
+    public String getName() {
+        return "obsidian";
+    }
+
+    @Override
+    public float getToughness() {
+        return 2.5f;
+    }
+}
