98e62948e091c0b4499c7e54aa650a9f40136f75
CamoMano <camomano99@gmail.com>
2020-04-07 18:22:25 -0400

    Added config

--- src/main/java/com/vanillaenhanced/VanillaEnhanced.java
+++ src/main/java/com/vanillaenhanced/VanillaEnhanced.java
@@ -1,9 +1,12 @@
 package com.vanillaenhanced;
 
+import com.vanillaenhanced.config.ModConfig;
 import com.vanillaenhanced.materials.items.ArmorMaterialEmerald;
 import com.vanillaenhanced.materials.items.ArmorMaterialObsidian;
 import com.vanillaenhanced.materials.items.ToolMaterialEmerald;
 import com.vanillaenhanced.materials.items.ToolMaterialObsidian;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import com.vanillaenhanced.items.*;
 import net.minecraft.entity.EquipmentSlot;
@@ -26,30 +29,44 @@ public class VanillaEnhanced implements ModInitializer {
     @Override
     public void onInitialize() {
         log(Level.INFO, "Hello there.");
-        //Obsidian Tools
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_pickaxe"), new PickaxeBase(new ToolMaterialObsidian()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_axe"), new AxeBase(new ToolMaterialObsidian()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_shovel"), new ShovelBase(new ToolMaterialObsidian()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_sword"), new SwordBase(new ToolMaterialObsidian()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_hoe"), new HoeBase(new ToolMaterialObsidian()));
-        //Obsidian Armor
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_helmet"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.HEAD));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_chestplate"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.CHEST));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_leggings"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.LEGS));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"obsidian_boots"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.FEET));
 
-        //Emerald Tools
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_pickaxe"), new PickaxeBase(new ToolMaterialEmerald()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_axe"), new AxeBase(new ToolMaterialEmerald()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_shovel"), new ShovelBase(new ToolMaterialEmerald()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_sword"), new SwordBase(new ToolMaterialEmerald()));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_hoe"), new HoeBase(new ToolMaterialEmerald()));
-        //Emerald Armor
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_helmet"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.HEAD));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_chestplate"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.CHEST));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_leggings"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.LEGS));
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID,"emerald_boots"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.FEET));
+        //Config
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+        if (config.enableobsidiangear == true) {
 
+            //Obsidian Tools
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_pickaxe"), new PickaxeBase(new ToolMaterialObsidian()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_axe"), new AxeBase(new ToolMaterialObsidian()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_shovel"), new ShovelBase(new ToolMaterialObsidian()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_sword"), new SwordBase(new ToolMaterialObsidian()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_hoe"), new HoeBase(new ToolMaterialObsidian()));
+            //Obsidian Armor
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_helmet"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.HEAD));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_chestplate"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.CHEST));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_leggings"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.LEGS));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "obsidian_boots"), new ArmorBase(OBSIDIAN_ARMOR, EquipmentSlot.FEET));
+        }else {
+            LOGGER.info("[" + MOD_NAME + "] " + "Obsidian Gear Disabled");
+        }
+
+
+        if (config.enableemeraldgear == true) {
+            //Emerald Tools
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_pickaxe"), new PickaxeBase(new ToolMaterialEmerald()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_axe"), new AxeBase(new ToolMaterialEmerald()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_shovel"), new ShovelBase(new ToolMaterialEmerald()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_sword"), new SwordBase(new ToolMaterialEmerald()));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_hoe"), new HoeBase(new ToolMaterialEmerald()));
+            //Emerald Armor
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_helmet"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.HEAD));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_chestplate"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.CHEST));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_leggings"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.LEGS));
+            Registry.register(Registry.ITEM, new Identifier(MOD_ID, "emerald_boots"), new ArmorBase(EMERALD_ARMOR, EquipmentSlot.FEET));
+        } else{
+            LOGGER.info("[" + MOD_NAME + "] " + "Emerald Gear Disabled");
+    }
+    LOGGER.info(config.enableobsidiangear);
     }
 
     public static void log(Level level, String message){
---
+++ src/main/java/com/vanillaenhanced/config/ModMenu.java
@@ -0,0 +1,27 @@
+package com.vanillaenhanced.config;
+
+
+import io.github.prospector.modmenu.api.ModMenuApi;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+
+@Environment(EnvType.CLIENT)
+public class ModMenu implements ModMenuApi {
+
+    @Override
+    public String getModId() {
+        return "vanillaenhanced";
+    }
+
+
+    @Override
+    public Optional<Supplier<Screen>> getConfigScreen(Screen screen) {
+        return Optional.of(AutoConfig.getConfigScreen(ModConfig.class, screen));
+    }
+
+}
