c9e445f04dccda1bee7adc22210bfdeef011a540
CamoMano <camomano99@gmail.com>
2020-08-13 00:47:41 -0400

    generation fixes

---
+++ src/main/resources/data/minecraft/dimension/overworld.json.d
@@ -0,0 +1,12 @@
+{
+    "type": "minecraft:overworld",
+    "generator": {
+        "biome_source": {
+            "type": "minecraft:fixed",
+            "biome": "vanillaenhanced:test_biome"
+        },
+        "seed": 0,
+        "settings": "minecraft:overworld",
+        "type": "minecraft:noise"
+    }
+}
