980081fc0d4535ef3c48da880866f95578d8c563
CamoMano <camomano99@gmail.com>
2020-08-14 22:14:13 -0400

    Fixed redwood sapling
    - Redwood trees now require 4 saplings

--- src/main/java/com/vanillaenhanced/world/feature/tree/RedwoodSaplingGenerator.java
+++ src/main/java/com/vanillaenhanced/world/feature/tree/RedwoodSaplingGenerator.java
@@ -1,18 +1,23 @@
 package com.vanillaenhanced.world.feature.tree;
 
 import com.vanillaenhanced.registry.ModInit;
-import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.block.sapling.LargeTreeSaplingGenerator;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Random;
 
-public class RedwoodSaplingGenerator extends SaplingGenerator {
+public class RedwoodSaplingGenerator extends LargeTreeSaplingGenerator {
+
 	@Nullable
+	@Override
+	protected ConfiguredFeature<TreeFeatureConfig, ?> createLargeTreeFeature(Random random) {
+		return null;
+	}
+
 	@Override
 	protected ConfiguredFeature<TreeFeatureConfig, ?> createTreeFeature(Random random, boolean bl) {
 		return ModInit.REDWOOD_TREE_FEATURE.configure(ModInit.REDWOOD_TREE_CONFIG);
 	}
-
 }
