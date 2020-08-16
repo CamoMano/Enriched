3c209b6e3785e20e634217a585c9b68c7d1c8275
CamoMano <camomano99@gmail.com>
2020-08-16 17:55:15 -0400

    Update ModConfig.java
    updated config backgrounds

--- src/main/java/com/vanillaenhanced/config/ModConfig.java
+++ src/main/java/com/vanillaenhanced/config/ModConfig.java
@@ -9,8 +9,8 @@ import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = "vanillaenhanced")
 @Config.Gui.CategoryBackground(category = "Biomes" , background = "vanillaenhanced:textures/block/redwood_planks.png")
-@Config.Gui.CategoryBackground(category = "Blocks" , background = "minecraft:textures/block/stone.png")
-@Config.Gui.CategoryBackground(category = "Items" , background = "vanillaenhanced:textures/block/sapphire_ore.png")
+@Config.Gui.CategoryBackground(category = "Blocks" , background = "vanillaenhanced:textures/block/marble.png")
+@Config.Gui.CategoryBackground(category = "Items" , background = "vanillaenhanced:textures/block/amethyst_ore.png")
     public class ModConfig implements ConfigData {
 
     //Items & Ores
