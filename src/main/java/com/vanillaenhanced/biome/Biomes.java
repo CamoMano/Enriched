a4cde95e76575992fecaa199889669e9d73afc5d
CamoMano <camomano99@gmail.com>
2020-08-23 12:49:22 -0400

    fixing biomes

--- src/main/java/com/vanillaenhanced/biome/Biomes.java
+++ src/main/java/com/vanillaenhanced/biome/Biomes.java
@@ -3,6 +3,9 @@ package com.vanillaenhanced.biome;
 import com.vanillaenhanced.mixin.BuiltinBiomesAccessor;
 import com.vanillaenhanced.world.Features;
 import com.vanillaenhanced.world.feature.tree.RedwoodTreeFeature;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.sound.BiomeMoodSound;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
@@ -14,25 +17,24 @@ import net.minecraft.world.biome.BiomeEffects;
 import net.minecraft.world.biome.GenerationSettings;
 import net.minecraft.world.biome.SpawnSettings;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.LakeFeature;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilders;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+import sun.jvm.hotspot.gc.shared.Generation;
 
 import static com.vanillaenhanced.VanillaEnhanced.MOD_ID;
 
 public final class Biomes {
     public static final RegistryKey<Biome> DIVERSE_FOREST_KEY;
     public static final RegistryKey<Biome> REDWOOD_FOREST_KEY;
-    //public static final RegistryKey<Biome> DESERT_MOUNTAINS_KEY;
+    public static final RegistryKey<Biome> DESERT_MOUNTAINS_KEY;
     //public static final RegistryKey<Biome> EXTREME_MOUNTAIN_KEY;
     //public static final RegistryKey<Biome> FROZEN_DESERT_KEY;
     //public static final RegistryKey<Biome> EXTREME_JUNGLE_KEY;
     //public static final RegistryKey<Biome> MONOLITH_KEY;
     public static final Biome DIVERSE_FOREST;
     public static final Biome REDWOOD_FOREST;
-    //public static final Biome DESERT_MOUNTAINS;
+    public static final Biome DESERT_MOUNTAINS;
     //public static final Biome EXTREME_MOUNTAIN;
     //public static final Biome FROZEN_DESERT;
     //public static final Biome EXTREME_JUNGLE;
@@ -42,14 +44,18 @@ public final class Biomes {
 
         Registry.register(BuiltinRegistries.BIOME, DIVERSE_FOREST_KEY.getValue(), DIVERSE_FOREST);
         Registry.register(BuiltinRegistries.BIOME, REDWOOD_FOREST_KEY.getValue(), REDWOOD_FOREST);
+        Registry.register(BuiltinRegistries.BIOME, DESERT_MOUNTAINS_KEY.getValue(), DESERT_MOUNTAINS);
         BuiltinBiomesAccessor.getIdMap().put(BuiltinRegistries.BIOME.getRawId(DIVERSE_FOREST), DIVERSE_FOREST_KEY);
         BuiltinBiomesAccessor.getIdMap().put(BuiltinRegistries.BIOME.getRawId(REDWOOD_FOREST), REDWOOD_FOREST_KEY);
+        BuiltinBiomesAccessor.getIdMap().put(BuiltinRegistries.BIOME.getRawId(DESERT_MOUNTAINS), DESERT_MOUNTAINS_KEY);
     }
+
 
 
     static {
         DIVERSE_FOREST_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID+":diverse_forest"));
         REDWOOD_FOREST_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID+":redwood_forest"));
+        DESERT_MOUNTAINS_KEY = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID+":desert_mountains"));
         DIVERSE_FOREST = new Biome.Builder()
                 .category(Biome.Category.FOREST)
                 .depth(0.05F)
@@ -94,6 +100,43 @@ public final class Biomes {
                         .build())
                 .temperature(0.8F)
                 .build();
+        DESERT_MOUNTAINS = new Biome.Builder()
+                .category(Biome.Category.DESERT)
+                .depth(1.0F)
+                .scale(0.75F)
+                .temperature(2.0F)
+                .downfall(0.0F)
+                .effects(new BiomeEffects.Builder()
+                        .waterColor(4159204)
+                        .waterFogColor(329011)
+                        .fogColor(12638463)
+                        .moodSound(BiomeMoodSound.CAVE)
+                        .skyColor(0x84AAFF)
+                        .grassColor(0x516b2e)
+                        .build())
+                .generationSettings(new GenerationSettings.Builder()
+                .surfaceBuilder(SurfaceBuilder.DEFAULT.method_30478(SurfaceBuilder.SAND_CONFIG))
+                        .structureFeature(ConfiguredStructureFeatures.DESERT_PYRAMID)
+                        .structureFeature(ConfiguredStructureFeatures.RUINED_PORTAL_DESERT)
+                        .structureFeature(ConfiguredStructureFeatures.VILLAGE_DESERT)
+                        .feature(GenerationStep.Feature.VEGETAL_DECORATION, ConfiguredFeatures.PATCH_CACTUS_DESERT)
+                        .feature(GenerationStep.Feature.VEGETAL_DECORATION, ConfiguredFeatures.PATCH_DEAD_BUSH)
+                        .build())
+                .precipitation(Biome.Precipitation.NONE)
+                .spawnSettings(new SpawnSettings.Builder()
+                .creatureSpawnProbability(0.2F)
+                        .spawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.RABBIT, 8, 2, 4))
+                        .spawn(SpawnGroup.AMBIENT, new SpawnSettings.SpawnEntry(EntityType.SPIDER, 100, 4, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.HUSK, 95, 4, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.ZOMBIE_VILLAGER, 5, 1, 1))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.SKELETON, 100, 4, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.CREEPER, 100, 4, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.SLIME, 100, 4, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.ENDERMAN, 10, 1, 4))
+                        .spawn(SpawnGroup.MONSTER, new SpawnSettings.SpawnEntry(EntityType.WITCH, 5, 1, 1))
+                        .build())
+                .build();
+
 
 
 
--- src/main/java/com/vanillaenhanced/biome/DesertMountains.java
+++ src/main/java/com/vanillaenhanced/biome/DesertMountains.java
@@ -2,8 +2,27 @@ package com.vanillaenhanced.biome;
 
 
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.ConfiguredFeatures;
+import net.minecraft.world.gen.feature.ConfiguredStructureFeatures;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilders;
+import sun.security.krb5.Config;
 
-public class DesertMountains {
+public class DesertMountains extends GenerationSettings.Builder {
+
+
+    public DesertMountains() {
+        GenerationSettings.Builder generationSettingsBuilder = (new GenerationSettings.Builder()).surfaceBuilder(ConfiguredSurfaceBuilders.DESERT);
+
+        generationSettingsBuilder.structureFeature(ConfiguredStructureFeatures.PILLAGER_OUTPOST);
+        generationSettingsBuilder.structureFeature(ConfiguredStructureFeatures.RUINED_PORTAL);
+        generationSettingsBuilder.build();
+
+
+
+    }
+
 /*
     public DesertMountains() {
             super((new Settings()).configureSurfaceBuilder(SurfaceBuilder.DEFAULT, SurfaceBuilder.SAND_CONFIG)
