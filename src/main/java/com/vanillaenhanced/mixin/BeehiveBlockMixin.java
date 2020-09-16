dd00014fbd62db2a77a81aa28136d3a515dc50e0
CamoMano <camomano99@gmail.com>
2020-09-16 00:33:02 -0400

    cleanup + language updates

--- src/main/java/com/vanillaenhanced/mixin/BeehiveBlockMixin.java
+++ src/main/java/com/vanillaenhanced/mixin/BeehiveBlockMixin.java
@@ -4,19 +4,12 @@ package com.vanillaenhanced.mixin;
 import com.vanillaenhanced.entity.HoneySlime;
 import net.minecraft.block.BeehiveBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -28,14 +21,14 @@ public class BeehiveBlockMixin {
     @Inject(method = "takeHoney(Lnet/minecraft/world/World;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "TAIL"))
     public void takeHoney(World world, BlockState state, BlockPos pos, CallbackInfo ci) {
         if (ThreadLocalRandom.current().nextInt(3) == 2) {
-            System.out.println("Yes");
+            //System.out.println("Yes");
             HoneySlime honeySlime = HONEY_SLIME.create(world);
             honeySlime.refreshPositionAndAngles((double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, 0.0F, 0.0F);
             honeySlime.setHealth(1f);
             world.spawnEntity(honeySlime);
             return;
         }
-        System.out.println("No");
+        //System.out.println("No");
     }
 
 }
