e2f29fd9fba6bc5f2237db59526efb5380603084
CamoMano <camomano99@gmail.com>
2020-09-16 00:17:56 -0400

    fixed Honey Slime spawns

--- src/main/java/com/vanillaenhanced/mixin/BeehiveBlockMixin.java
+++ src/main/java/com/vanillaenhanced/mixin/BeehiveBlockMixin.java
@@ -24,11 +24,10 @@ import static com.vanillaenhanced.registry.ModInit.HONEY_SLIME;
 
 
 @Mixin(BeehiveBlock.class)
-/*
 public class BeehiveBlockMixin {
-    @Inject(method = "takeHoney(Lnet/minecraft/world/World;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "INVOKE_ASSIGN"))
+    @Inject(method = "takeHoney(Lnet/minecraft/world/World;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "TAIL"))
     public void takeHoney(World world, BlockState state, BlockPos pos, CallbackInfo ci) {
-        if ((ThreadLocalRandom.current().nextInt(6) + 1) % 2 == 0) {
+        if (ThreadLocalRandom.current().nextInt(3) == 2) {
             System.out.println("Yes");
             HoneySlime honeySlime = HONEY_SLIME.create(world);
             honeySlime.refreshPositionAndAngles((double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, 0.0F, 0.0F);
@@ -41,21 +40,3 @@ public class BeehiveBlockMixin {
 
 }
 
- */
-public class BeehiveBlockMixin {
-    @Inject(method = "onUse(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Lnet/minecraft/util/ActionResult;", at = @At(value = "INVOKE_ASSIGN"))
-    public void onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir) {
-        ItemStack itemStack = player.getStackInHand(hand);
-        if (itemStack.getItem() != Items.GLASS_BOTTLE) return;
-        if ((ThreadLocalRandom.current().nextInt(6) + 1) % 2 == 0) {
-            System.out.println("Yes");
-            HoneySlime honeySlime = HONEY_SLIME.create(world);
-            honeySlime.refreshPositionAndAngles((double) pos.getX() + 0.5D, (double) pos.getY(), (double) pos.getZ() + 0.5D, 0.0F, 0.0F);
-            honeySlime.setHealth(1f);
-            world.spawnEntity(honeySlime);
-            return;
-        }
-        System.out.println("No");
-    }
-
-}
