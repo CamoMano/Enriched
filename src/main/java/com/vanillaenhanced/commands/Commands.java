5c19c31e56cab2dfce6f8ab3294f039d4e0d7e40
CamoMano <camomano99@gmail.com>
2021-07-18 18:17:44 -0400

    added version command
    /ve version

--- src/main/java/com/vanillaenhanced/commands/Commands.java
+++ src/main/java/com/vanillaenhanced/commands/Commands.java
@@ -3,6 +3,7 @@ package com.vanillaenhanced.commands;
 import com.mojang.brigadier.context.CommandContext;
 import com.vanillaenhanced.VanillaEnhanced;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.text.Text;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
@@ -12,7 +13,7 @@ public class Commands {
             dispatcher.register(literal("ve")
                 .then(literal("version")
                     .executes(context -> {
-                        System.out.println(VanillaEnhanced.MOD_NAME + " " + VanillaEnhanced.MOD_VERSION);
+                        context.getSource().sendFeedback(Text.of(VanillaEnhanced.MOD_NAME + " " + VanillaEnhanced.MOD_VERSION), false);
                         return 1;
                     })));
         });
