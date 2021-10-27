b5b6b702c135a075f10a23e636c99b4922b9785b
CamoMano <camomano99@gmail.com>
2021-10-27 18:21:23 -0400

    updates
    - updated fabric and loom
    - code cleanup

--- src/main/java/com/vanillaenhanced/blocks/StairsBase.java
+++ src/main/java/com/vanillaenhanced/blocks/StairsBase.java
@@ -1,7 +1,5 @@
 package com.vanillaenhanced.blocks;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
 
--- src/main/java/com/vanillaenhanced/blocks/WoodDoorBase.java
+++ src/main/java/com/vanillaenhanced/blocks/WoodDoorBase.java
@@ -1,7 +1,5 @@
 package com.vanillaenhanced.blocks;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.DoorBlock;
 
 public class WoodDoorBase extends DoorBlock {
