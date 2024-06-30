a96eb59309758c3e3f060afa89ab5ba01ef8edc9
Ayydxn <arcanewastakenofficial@gmail.com>
2024-06-30 19:20:49 -0400

    Setup new Fabric project
    - Setup a new Fabric project for the rewrite.

---
+++ src/main/java/com/enrichedmc/enriched/EnrichedDataGenerator.java
@@ -0,0 +1,13 @@
+package com.enrichedmc.enriched;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnrichedDataGenerator implements DataGeneratorEntrypoint
+{
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+    {
+
+    }
+}
---
+++ src/main/java/com/enrichedmc/enriched/EnrichedMod.java
@@ -0,0 +1,19 @@
+package com.enrichedmc.enriched;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
+public class EnrichedMod implements ModInitializer
+{
+    public static final Logger LOGGER = (Logger) LogManager.getLogger("Enriched");
+    public static final String MOD_ID = "enriched";
+
+    @Override
+    public void onInitialize()
+    {
+        LOGGER.info("Initializing Enriched... (Version: {})", FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(NullPointerException::new)
+                .getMetadata().getVersion().getFriendlyString());
+    }
+}
